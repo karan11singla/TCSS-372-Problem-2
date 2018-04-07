7db26c6bc60230cce6395a61333051b7ab958f4b
enochchan2010 <32918985+enochchan2010@users.noreply.github.com>
2018-04-07 16:36:50 -0700

    ADD and AND instructions so far

--- lc3.c
+++ lc3.c
@@ -1,33 +1,27 @@
 // lc3.c
 
+
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+#include "lc3.h"
 // you can define a simple memory module here for this program
 unsigned short memory[32]; // 32 words of memory enough to store simple program
 
-int controller (CPU_p cpu) {
+void controller () {
     // check to make sure both pointers are not NULL
     // do any initializations here
-    int r0[16];
-    int r1[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1}; // 0x0005
-    int r2[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1}; // 0x000F
-    int r3[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // 0x0000
-    int r4[16];
-    int r5[16];
-    int r6[16];
-    int r7[16];
-    unsigned int pc = 0;
-    unsigned int ir = 0;
-    unsigned short ir[16];
-    unsigned int opcode, Rd, Rs1, Rs2, immed-offset;// fields for the IR
-    state = FETCH;
+        unsigned int opcode, Rd, Rs1, Rs2, offset ;// fields for the IR
+
+    int state = FETCH;
     // for (;;) { // efficient endless loop to be used in the next problem
         switch (state) {
             case FETCH: // microstates 18, 33, 35 in the book
                 printf("Here in FETCH\n");
-                ir = memory[pc];
-                pc += 1;
                 // get memory[PC] into IR - memory is a global array
                 // increment PC
-                printf("Contents of IR = %04X\n", cpu->ir);
+                //printf("Contents of IR = %04X\n", ir);
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // put printf statements in each state and microstate to see that it is
 // working
@@ -35,7 +29,6 @@ int controller (CPU_p cpu) {
                 state = DECODE;
                 break;
             case DECODE: // microstate 32
-                opcode =
                 // get the fields out of the IR
                 // make sure opcode is in integer form
                 // hint: use four unsigned int variables, opcode, Rd, Rs, and
@@ -71,11 +64,74 @@ int controller (CPU_p cpu) {
                 break;
             case STORE: // Look at ST. Microstate 16 is the store to memory
                 switch (opcode) {
-                    // write back to register or store MDR into memory
+                    // do what the opcode is for, e.g. ADD
+                    // in case of TRAP: call trap(int trap_vector) routine,
+                    // see below for TRAP x25 (HALT)
                 }
-                // do any clean up here in prep for the next complete cycle
-                state = FETCH;
+                state = STORE;
                 break;
         }
     // if-loop }
+}
+
+int main(int argc, char* argv[]) {
+    //memory[0] = argv[1];
+    printf("Here");
+
+}
+
+unsigned int intToBinary(unsigned int num) {
+    if (num == 0) return 0;
+    return (num % 2) + 10 * intToBinary(num / 2);
+}
+
+unsigned int * toArray(unsigned int num) {
+    int length = log10(num) + 1;
+    unsigned int *arr = calloc(16, sizeof(unsigned int));
+    int i;
+    for (i = 15; i > 15 - length; i--) {
+        arr[i] = num % 10;
+        num /= 10;
+    }
+    return arr;
+}
+
+void executeAdd(unsigned int Rd, unsigned int Rs1, unsigned int Rs2,
+                unsigned int offset, unsigned int mode) {
+    if (mode == 0) { // need to parse mode from IR
+        Rd = Rs1 + Rs2;
+    } else {
+        Rd = Rs1 + offset;
+    }
+    // need to set condition codes
+}
+
+void executeAnd(unsigned int Rd, unsigned int Rs1, unsigned int Rs2,
+                unsigned int offset, unsigned int mode) {
+    unsigned int s1Bin = intToBinary(Rs1);
+    unsigned int *s1Arr = toArray(s1Bin);
+    unsigned int result;
+    int i;
+    if (mode == 0) { // need to parse mode from IR
+        unsigned int s2Bin = intToBinary(Rs2);
+        unsigned int *s2Arr = toArray(s2Bin);
+        for (i = 15; i >= 0; i--) {
+            if (s1Arr[i] == 1 && s2Arr[i] == 1) {
+                result += pow(10, 15 - i);
+            }
+        }
+    } else {
+        unsigned int imm5Bin = intToBinary(offset);
+        unsigned int *imm5Arr = toArray(imm5Bin);
+        for (i = 15; i >= 11; i--) {
+            if (s1Arr[i] == 1 && imm5Arr[i] == 1) {
+                result += pow(10, 15 - i);
+            }
+        }
+    }
+
+    // convert back from binary to integer
+    // store into Rd
+
+    // need to set condition codes
 }
